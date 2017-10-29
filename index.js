8bc5083ad15f0ff0430f71f179aef666b923a86a
S.Hafezi <salar.hfz@gmail.com>
2017-10-29 18:51:15 +0330

    new feature adds functionality to run shell commands and use ipc

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const electron = require('electron')
 const BrowserWindow = electron.BrowserWindow
 const app = electron.app
+const ipc = electron.ipcMain
 
 app.on('ready', () => {
-	let appWindow = new BrowserWindow()
+	let appWindow = new BrowserWindow({
+		show: false
+	})
 	appWindow.loadURL('https://www.google.com')
 
 	let infoWindow = new BrowserWindow({
 		width: 400,
 		height: 300,
-		frame: false,
-		transparent: true
+		//frame: false,
+		//transparent: true,
+		show: false
 	})
 	infoWindow.loadURL('file://' + __dirname + '/info.html')
+
+	appWindow.once('ready-to-show', () => {
+		appWindow.show()
+		setTimeout(() => {
+			infoWindow.show()
+		}, 1000)
+	})
+	
+	ipc.on('closeInfoWindow', (event, arg) => {
+		event.returnValue = ''
+		console.log('hi')
+		infoWindow.hide()
+	})
 })
