0ba76c2d4bdd6072357f5351961e903798f0f9da
S.Hafezi <salar.hfz@gmail.com>
2017-10-29 18:19:07 +0330

    new feature adds app.css adds info.html updates index.js to show the other window

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ const app = electron.app
 app.on('ready', () => {
 	let appWindow = new BrowserWindow()
 	appWindow.loadURL('https://www.google.com')
+
+	let infoWindow = new BrowserWindow({
+		width: 400,
+		height: 300,
+		frame: false,
+		transparent: true
+	})
+	infoWindow.loadURL('file://' + __dirname + '/info.html')
 })
